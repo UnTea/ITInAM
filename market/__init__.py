bac101c7c29b6704cb019ccaff0c907121d40236
UnTea <weqqu2014@yandex.ru>
2023-12-15 20:36:39 +0300

    add authorization

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '76318f7c552db5633d735d73ca2f42103b7ee3ec7628394f'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
