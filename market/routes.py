e62d9d48b9fbb84279120cbfdca1a321f0f15100
UnTea <weqqu2014@yandex.ru>
2023-12-15 20:52:18 +0300

    add logout functional

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -13,6 +13,7 @@ def home_page():
 
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
 
@@ -32,10 +33,11 @@ def register_page():
 
         db.session.add(user_to_create)
         db.session.commit()
-
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
 
-    if form.errors != {}:
+    if form.errors != {}:  # If there are not errors from the validations
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
@@ -58,3 +60,11 @@ def login_page():
             flash('Username and password are not match! Please try again', category='danger')
 
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+
+    return redirect(url_for("home_page"))
