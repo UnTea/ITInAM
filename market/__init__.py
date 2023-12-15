94541f7b3f1cfc26d13ee4884809c610965cb1ef
UnTea <weqqu2014@yandex.ru>
2023-12-15 20:26:27 +0300

    add subauthorization

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '76318f7c552db5633d735d73ca2f42103b7ee3ec7628394f'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
